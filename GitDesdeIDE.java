e6858f14e9149fcd694d9545a21d2fb1dd5ca4d8
ikerodri <39056860+ikerodri@users.noreply.github.com>
2018-10-24 19:00:00 +0200

    Update GitDesdeIDE.java

--- GitDesdeIDE.java
+++ GitDesdeIDE.java
@@ -1,6 +1,6 @@
 public class GitDesdeIDE {
     public static void main(String[] args) {
         System.out.println("Hola que tal?");
-        System.out.println("Molt bé, gràcies.");
+        System.out.println("Malament, gràcies.");
     }
 }
